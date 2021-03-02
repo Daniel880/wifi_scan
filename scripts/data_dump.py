80df03c8e797e4cc809839d62727d71faf0f6058
Daniel Wyrwał <daniel.wyrwal@gmail.com>
2021-03-02 18:31:06 +0100

    Added data dump with map -> base_link transform

--- scripts/data_dump.py
+++ scripts/data_dump.py
@@ -19,32 +19,68 @@
 import rospy
 from wifi_scan.msg import Fingerprint
 from std_msgs.msg import Header
+import tf2_ros
 
-def fingerprintCallback(fingerprint, f):
-  rospy.loginfo(rospy.get_name() + ": I heard something")
-  f.write(str(fingerprint.header.stamp) + ',')
+lista_mac = []
+
+
+def fingerprintCallback(fingerprint, args):
+  f_h = args[0]
+  f_d = args[1]
+  tfBuffer = args[2]
+  data = dict()
+
+  try:
+    trans = tfBuffer.lookup_transform("map", 'base_footprint', rospy.Time())
+    x = trans.transform.translation.x
+    y = trans.transform.translation.y
+    z = trans.transform.rotation.z
+  except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+    rospy.loginfo(rospy.get_name() + ": Exception")
+
+  #f.write(str(fingerprint.header.stamp) + ',')
   for address_rssi in fingerprint.list:
-    values = address_rssi.address
-    values += ','
-    values += str(address_rssi.rssi)
-    values +=','
-    f.write(values)
-  f.write('\n')
+    data[address_rssi.address] = address_rssi.rssi
+    if address_rssi.address not in lista_mac:
+        lista_mac.append(address_rssi.address)
+        rospy.loginfo(rospy.get_name() + ": Dodano adres do listy: " + address_rssi.address)
+        string = address_rssi.address
+        string += ','
+        f_h.write(string)
+
+  string = '{},{},{},'.format(x, y, z)
+  for address in lista_mac:
+    string += str(data.get(address, ""))
+    string += ','
+  f_d.write(string)
+  f_d.write('\n')
 
 def groundTruthCallback(data, f):
   rospy.loginfo(rospy.get_name() + ": I heard something")
-  f.write(str(data.stamp) + ',' + data.frame_id)
-  f.write('\n')
+  f_d.write(str(data.stamp) + ',' + data.frame_id)
+  f_d.write('\n')
 
 def data_dump():
   rospy.init_node('data_dump')
-  f = open('dump.csv', 'w')
-  f.write('')
-  f.close()
-  f = open('dump.csv', 'a')
-  rospy.Subscriber("/wifi_fp", Fingerprint, fingerprintCallback, f)
-  rospy.Subscriber("/gt_nfc", Header, groundTruthCallback, f)
+
+  f_h = open('HEADER.csv', 'w')
+  f_d = open('DATA.csv', 'w')
+  f_h.write('')
+  f_d.write('')
+  f_h.close()
+  f_d.close()
+  f_h = open('HEADER.csv', 'a')
+  f_d = open('DATA.csv', 'a')
+  string = "x,y,z,"
+  f_h.write(string)
+
+  tfBuffer = tf2_ros.Buffer()
+  listener = tf2_ros.TransformListener(tfBuffer)
+
+  rospy.Subscriber("/wifi_fp", Fingerprint, fingerprintCallback, [f_h, f_d,tfBuffer])
+  rospy.Subscriber("/gt_nfc", Header, groundTruthCallback, [f_h, f_d])
   rospy.spin()
+
 
 if __name__ == '__main__':
   data_dump()
